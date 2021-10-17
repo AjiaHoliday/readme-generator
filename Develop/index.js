8046923a102e6557aad253c8f1ae1c056606cfc9
ajia <ajialeeholiday@gmail.com>
2021-10-16 21:31:29 -0600

    markdown started

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,42 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-    {   
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const api = require('./utils/api.js');
+
+// array of questions for user input
+const questions = ["What is the title of your project? (required)",
+"What is your GitHub username? (required)",
+"What is the name of your GitHub repo? (require)",
+"Please provide a description of this project. (required)",
+'Provide a link to your project. (required)',
+"Please describe how to use this project with instructions and examples. (required)",
+"If applicable, how do you install this application?",
+"If applicable, how can others contribute?",
+"If applicable, provide examples of tests and examples on how to run them.",
+"Choose a license for your project.",
+];
+
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            throw err;
+        }
+        console.log('README successfully saved!')
+    })
+};
+
+// function to initialize app
+function init() {
+    const [projectTitle, username, repo, description, usage, installation, contributes, tests, license
+    ] = questions;
+
+    return inquirer.prompt([{   
         type: 'input',
-        message: "What is the title of your project? (required)",
+        message: projectTitle,
         name:'projectTitle',
         default: 'Project Title',
         validate: projectTitleInput => {
@@ -21,7 +50,7 @@ const questions = () => {
     },
     {
         type: 'input',
-        message: "What is your GitHub username? (required)",
+        message: username,
         name: 'username',
         default: 'AjiaHoliday',
         validate: usernameInput => {
@@ -35,24 +64,77 @@ const questions = () => {
     },
     {
         type: 'input',
-        message: "What is the name of your GitHub repo? (require)",
+        message: repo,
         name: 'repo',
-        
+        default: 'readme-generator',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('A valid GitHub repo is requred');
+                return false;
+            }
+        }
+
     },
-    "Please provide a description of this project. (required)",
-    "If applicable, how do you install this application?",
-    "Please describe how to use this project with instructions and examples. (required)",
-    "If applicable, how can others contribute?",
-    "If applicable, provide examples of tests and examples on how to run them.",
-    "Choose a license for your project.",
-]);
-}
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
+    {
+        type: 'input',
+        mesage: description,
+        name: 'description',
+        default: 'Project Description',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'link',
+        name: 'link',
+        message: link,
+        validate: linkInput => {
+            if(linkInput){
+                return true;
+            } else{
+                console.log("Please provide a link to your project.");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: installation,
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: usage,
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: contributes,
+        name: 'contributes'
+    },
+    {
+        type: 'input',
+        message: tests,
+        name: 'tests'
+    },
+    {
+        name: 'list',
+        message: license,
+        name: 'license',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense']
+    }
+])
+    .then(data => generateMarkdown(data))
+    .then(pageLayout => writeToFile("README.md", pageLayout))
+        
+};
+    
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const {userInfo} = require('os');
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  };
+  return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`
+}
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license) {
+    return '';
+  }
+  return `[Learn More about ${license}](https://choosealicense.com/licenses/${license})`
+}
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license) {
+    return '';
+  };
+  return `
+  ## License
+  
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}`
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+// function to generate markdown for README
+function generateMarkdown(userResponses, userInfo) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Description](#description)
+  *[In]
 
 `;
 }
